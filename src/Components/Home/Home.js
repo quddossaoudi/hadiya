958c208e9fccd6d95084d9f9dcc805d63b592a8c
quddossaoudi <quddossaoudi@gmail.com>
2023-04-24 16:56:43 +0800

    gsap animation added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import css from "./Home.module.css";
 
 function Home() {
-  return <div>Home</div>;
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-300vw",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+
+    return () => {
+      pin.kill();
+    };
+  }, []);
+
+  return (
+    <section className={css.scrollSectionOuter}>
+      <div ref={triggerRef}>
+        <div ref={sectionRef} className={css.scrollSectionInner}>
+          <div className={css.scrollSection}>
+            <h3>section 1</h3>
+          </div>
+          <div className={css.scrollSection}>
+            <h3>section 2</h3>
+          </div>
+          <div className={css.scrollSection}>
+            <h3>section 3</h3>
+          </div>
+          <div className={css.scrollSection}>
+            <h3>section 4</h3>
+          </div>
+          <div className={css.scrollSection}>
+            <h3>section 5</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
--- src/Container/Main/Main.js
+++ src/Container/Main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Home from "../../Components/Home/Home";
 
 function Main() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
