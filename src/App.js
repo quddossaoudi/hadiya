cdff4e062560918f908e6003b08212fcf65bb548
quddossaoudi <quddossaoudi@gmail.com>
2023-04-24 14:54:20 +0800

    app directories changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+import css from "./App.module.css";
+import BarLoader from "react-spinners/BarLoader";
+import TypewriterComponent from "typewriter-effect";
+import Home from "../src/containers/Home";
 
 function App() {
+  // const [loading, setLoading] = useState(false);
+  // const [barWidth, setBarWidth] = useState(150);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //     setTimeout(() => {
+  //       setBarWidth(150);
+  //     }, 7000);
+  //   }, 8000);
+  //   setTimeout(() => {
+  //     setBarWidth(window.innerWidth);
+  //   }, 7400);
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {loading ? (
+        <>
+          <p className={css.introName}>
+            <TypewriterComponent
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDelay(75)
+                  .changeDeleteSpeed(75)
+                  .typeString("Hadiya Ali")
+                  .changeDelay(75)
+                  .changeDeleteSpeed(75)
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("하디야 알리")
+                  .changeDelay(75)
+                  .changeDeleteSpeed(75)
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("هدية علي")
+                  .start();
+              }}
+            />
+          </p>
+          <BarLoader
+            color="#000"
+            cssOverride={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              transition: "width 0.5s ease-out",
+            }}
+            speedMultiplier={1.2}
+            width={barWidth}
+            loading={loading}
+            height={1}
+          />
+        </> */}
+      {/* ) : ( */}
+      <Home />
+      {/* )} */}
     </div>
   );
 }
