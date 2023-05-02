bd280fd4f40a15d78b81f6e153540203367f63c8
quddossaoudi <quddossaoudi@gmail.com>
2023-05-02 14:33:34 +0800

    mobile view development

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   Menu,
   MenuButton,
@@ -10,7 +10,19 @@ import {
   useColorMode,
   Text,
   Image,
+  useBreakpointValue,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Input,
 } from "@chakra-ui/react";
+import { FiMenu } from "react-icons/fi";
 import css from "./Navbar.module.css";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsFacebook } from "react-icons/bs";
@@ -22,6 +34,9 @@ import LogoWhite from "../../assets/Logo/logoWhite.png";
 
 function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
 
   return (
     <>
@@ -33,82 +48,10 @@ function Navbar() {
             <Image src={LogoWhite} w="40px" h="40px" />
           )}
         </div>
-        <div className={css.listItem}>
-          <ul>
-            <a href="/">
-              <Text
-                color="Gray"
-                _hover={
-                  colorMode === "light"
-                    ? { color: "Black" }
-                    : { color: "White" }
-                }
-              >
-                Home
-              </Text>
-            </a>
-            <a href="#">
-              <Menu>
-                <MenuButton
-                  color="Gray"
-                  _hover={
-                    colorMode === "light"
-                      ? { color: "Black" }
-                      : { color: "White" }
-                  }
-                >
-                  Projects
-                </MenuButton>
-                <Portal>
-                  <MenuList border="none">
-                    <MenuItem className={css.MenuItem}>
-                      <a href="/weddings">
-                        <Text
-                          color="Gray"
-                          _hover={
-                            colorMode === "light"
-                              ? { color: "Black" }
-                              : { color: "White" }
-                          }
-                        >
-                          Weddings
-                        </Text>
-                      </a>
-                    </MenuItem>
-                    <MenuItem className={css.MenuItem}>
-                      <a href="/products">
-                        <Text
-                          color="Gray"
-                          _hover={
-                            colorMode === "light"
-                              ? { color: "Black" }
-                              : { color: "White" }
-                          }
-                        >
-                          Brands
-                        </Text>
-                      </a>
-                    </MenuItem>
-                    <MenuItem className={css.MenuItem}>
-                      <a href="/food">
-                        <Text
-                          color="Gray"
-                          _hover={
-                            colorMode === "light"
-                              ? { color: "Black" }
-                              : { color: "White" }
-                          }
-                        >
-                          Food
-                        </Text>
-                      </a>
-                    </MenuItem>
-                  </MenuList>
-                </Portal>
-              </Menu>
-            </a>
-            <span>
-              <a href="/about">
+        {isDesktop ? (
+          <div className={css.listItem}>
+            <ul>
+              <a href="/">
                 <Text
                   color="Gray"
                   _hover={
@@ -117,12 +60,108 @@ function Navbar() {
                       : { color: "White" }
                   }
                 >
-                  About
+                  Home
                 </Text>
               </a>
-            </span>
-            <a href="/equipments">
-              <Text
+              <a href="#">
+                <Menu>
+                  <MenuButton
+                    color="Gray"
+                    _hover={
+                      colorMode === "light"
+                        ? { color: "Black" }
+                        : { color: "White" }
+                    }
+                  >
+                    Projects
+                  </MenuButton>
+                  <Portal>
+                    <MenuList border="none">
+                      <MenuItem className={css.MenuItem}>
+                        <a href="/weddings">
+                          <Text
+                            color="Gray"
+                            _hover={
+                              colorMode === "light"
+                                ? { color: "Black" }
+                                : { color: "White" }
+                            }
+                          >
+                            Weddings
+                          </Text>
+                        </a>
+                      </MenuItem>
+                      <MenuItem className={css.MenuItem}>
+                        <a href="/products">
+                          <Text
+                            color="Gray"
+                            _hover={
+                              colorMode === "light"
+                                ? { color: "Black" }
+                                : { color: "White" }
+                            }
+                          >
+                            Brands
+                          </Text>
+                        </a>
+                      </MenuItem>
+                      <MenuItem className={css.MenuItem}>
+                        <a href="/food">
+                          <Text
+                            color="Gray"
+                            _hover={
+                              colorMode === "light"
+                                ? { color: "Black" }
+                                : { color: "White" }
+                            }
+                          >
+                            Food
+                          </Text>
+                        </a>
+                      </MenuItem>
+                    </MenuList>
+                  </Portal>
+                </Menu>
+              </a>
+              <span>
+                <a href="/about">
+                  <Text
+                    color="Gray"
+                    _hover={
+                      colorMode === "light"
+                        ? { color: "Black" }
+                        : { color: "White" }
+                    }
+                  >
+                    About
+                  </Text>
+                </a>
+              </span>
+              <a href="/equipments">
+                <Text
+                  color="Gray"
+                  _hover={
+                    colorMode === "light"
+                      ? { color: "Black" }
+                      : { color: "White" }
+                  }
+                >
+                  Equipments
+                </Text>
+              </a>
+              <a href="/testimonials">
+                <Text
+                  color="Gray"
+                  _hover={
+                    colorMode === "light"
+                      ? { color: "Black" }
+                      : { color: "White" }
+                  }
+                >
+                  Testimonials
+                </Text>
+              </a>
+              <Box
                 color="Gray"
                 _hover={
                   colorMode === "light"
@@ -130,11 +169,14 @@ function Navbar() {
                     : { color: "White" }
                 }
               >
-                Equipments
-              </Text>
-            </a>
-            <a href="/testimonials">
-              <Text
+                <a
+                  href="https://www.facebook.com/hadiyasphotography/"
+                  target="_blank"
+                >
+                  <BsFacebook />
+                </a>
+              </Box>
+              <Box
                 color="Gray"
                 _hover={
                   colorMode === "light"
@@ -142,61 +184,246 @@ function Navbar() {
                     : { color: "White" }
                 }
               >
-                Testimonials
-              </Text>
-            </a>
-            <Box
-              color="Gray"
-              _hover={
-                colorMode === "light" ? { color: "Black" } : { color: "White" }
-              }
-            >
-              <a
-                href="https://www.facebook.com/hadiyasphotography/"
-                target="_blank"
+                <a
+                  href="https://www.instagram.com/hadiyaali_photography/"
+                  target="_blank"
+                >
+                  <AiFillInstagram />
+                </a>
+              </Box>
+              <Box
+                color="Gray"
+                _hover={
+                  colorMode === "light"
+                    ? { color: "Black" }
+                    : { color: "White" }
+                }
+                className={colorMode === "light" ? css.Icon : css.IconDarkMode}
               >
-                <BsFacebook />
-              </a>
-            </Box>
-            <Box
-              color="Gray"
-              _hover={
-                colorMode === "light" ? { color: "Black" } : { color: "White" }
-              }
-            >
-              <a
-                href="https://www.instagram.com/hadiyaali_photography/"
-                target="_blank"
+                <a href="https://www.tiktok.com/@ali.hadiya" target="_blank">
+                  <FaTiktok />
+                </a>
+              </Box>
+              <Box
+                color="Gray"
+                _hover={
+                  colorMode === "light"
+                    ? { color: "Black" }
+                    : { color: "White" }
+                }
               >
-                <AiFillInstagram />
-              </a>
-            </Box>
-            <Box
-              color="Gray"
-              _hover={
-                colorMode === "light" ? { color: "Black" } : { color: "White" }
-              }
-              className={colorMode === "light" ? css.Icon : css.IconDarkMode}
-            >
-              <a href="https://www.tiktok.com/@ali.hadiya" target="_blank">
-                <FaTiktok />
-              </a>
-            </Box>
-            <Box
-              color="Gray"
-              _hover={
-                colorMode === "light" ? { color: "Black" } : { color: "White" }
-              }
-            >
-              <a href="https://wa.me/+60167518350" target="_blank">
-                <BsWhatsapp />
-              </a>
-            </Box>
+                <a href="https://wa.me/+60167518350" target="_blank">
+                  <BsWhatsapp />
+                </a>
+              </Box>
+              <Box
+                className={css.box}
+                cursor="pointer"
+                onClick={toggleColorMode}
+              >
+                {colorMode === "light" ? <MdSunny /> : <BsMoon />}
+              </Box>
+            </ul>
+          </div>
+        ) : (
+          <>
             <Box className={css.box} cursor="pointer" onClick={toggleColorMode}>
               {colorMode === "light" ? <MdSunny /> : <BsMoon />}
             </Box>
-          </ul>
-        </div>
+            <IconButton
+              onClick={onOpen}
+              variant="ghost"
+              icon={<FiMenu fontSize="1.25rem" />}
+              aria-label="Open Menu"
+            />
+            <>
+              <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+              >
+                <DrawerOverlay />
+                <DrawerContent>
+                  <ul>
+                    <a href="/">
+                      <Text
+                        color="Gray"
+                        _hover={
+                          colorMode === "light"
+                            ? { color: "Black" }
+                            : { color: "White" }
+                        }
+                      >
+                        Home
+                      </Text>
+                    </a>
+                    <a href="#">
+                      <Menu>
+                        <MenuButton
+                          color="Gray"
+                          _hover={
+                            colorMode === "light"
+                              ? { color: "Black" }
+                              : { color: "White" }
+                          }
+                        >
+                          Projects
+                        </MenuButton>
+                        <Portal>
+                          <MenuList border="none">
+                            <MenuItem className={css.MenuItem}>
+                              <a href="/weddings">
+                                <Text
+                                  color="Gray"
+                                  _hover={
+                                    colorMode === "light"
+                                      ? { color: "Black" }
+                                      : { color: "White" }
+                                  }
+                                >
+                                  Weddings
+                                </Text>
+                              </a>
+                            </MenuItem>
+                            <MenuItem className={css.MenuItem}>
+                              <a href="/products">
+                                <Text
+                                  color="Gray"
+                                  _hover={
+                                    colorMode === "light"
+                                      ? { color: "Black" }
+                                      : { color: "White" }
+                                  }
+                                >
+                                  Brands
+                                </Text>
+                              </a>
+                            </MenuItem>
+                            <MenuItem className={css.MenuItem}>
+                              <a href="/food">
+                                <Text
+                                  color="Gray"
+                                  _hover={
+                                    colorMode === "light"
+                                      ? { color: "Black" }
+                                      : { color: "White" }
+                                  }
+                                >
+                                  Food
+                                </Text>
+                              </a>
+                            </MenuItem>
+                          </MenuList>
+                        </Portal>
+                      </Menu>
+                    </a>
+                    <span>
+                      <a href="/about">
+                        <Text
+                          color="Gray"
+                          _hover={
+                            colorMode === "light"
+                              ? { color: "Black" }
+                              : { color: "White" }
+                          }
+                        >
+                          About
+                        </Text>
+                      </a>
+                    </span>
+                    <a href="/equipments">
+                      <Text
+                        color="Gray"
+                        _hover={
+                          colorMode === "light"
+                            ? { color: "Black" }
+                            : { color: "White" }
+                        }
+                      >
+                        Equipments
+                      </Text>
+                    </a>
+                    <a href="/testimonials">
+                      <Text
+                        color="Gray"
+                        _hover={
+                          colorMode === "light"
+                            ? { color: "Black" }
+                            : { color: "White" }
+                        }
+                      >
+                        Testimonials
+                      </Text>
+                    </a>
+                    <Box
+                      color="Gray"
+                      _hover={
+                        colorMode === "light"
+                          ? { color: "Black" }
+                          : { color: "White" }
+                      }
+                    >
+                      <a
+                        href="https://www.facebook.com/hadiyasphotography/"
+                        target="_blank"
+                      >
+                        <BsFacebook />
+                      </a>
+                    </Box>
+                    <Box
+                      color="Gray"
+                      _hover={
+                        colorMode === "light"
+                          ? { color: "Black" }
+                          : { color: "White" }
+                      }
+                    >
+                      <a
+                        href="https://www.instagram.com/hadiyaali_photography/"
+                        target="_blank"
+                      >
+                        <AiFillInstagram />
+                      </a>
+                    </Box>
+                    <Box
+                      color="Gray"
+                      _hover={
+                        colorMode === "light"
+                          ? { color: "Black" }
+                          : { color: "White" }
+                      }
+                      className={
+                        colorMode === "light" ? css.Icon : css.IconDarkMode
+                      }
+                    >
+                      <a
+                        href="https://www.tiktok.com/@ali.hadiya"
+                        target="_blank"
+                      >
+                        <FaTiktok />
+                      </a>
+                    </Box>
+                    <Box
+                      color="Gray"
+                      _hover={
+                        colorMode === "light"
+                          ? { color: "Black" }
+                          : { color: "White" }
+                      }
+                    >
+                      <a href="https://wa.me/+60167518350" target="_blank">
+                        <BsWhatsapp />
+                      </a>
+                    </Box>
+                  </ul>
+
+                  <DrawerCloseButton />
+                </DrawerContent>
+              </Drawer>
+            </>
+          </>
+        )}
       </div>
     </>
   );
